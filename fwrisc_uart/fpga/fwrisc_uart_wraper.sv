49ecc5a42e4bd74209e45bc26d933019a3ddebe2
yuzhenbo <yuzhenbo@44001a82-e59c-49f1-a214-40ce179027c6>
2019-07-18 08:32:54 +0000

    加入vivado工程 zybo开发板 75MHz
    git-svn-id: svn://144.34.249.81/repos0_proj/repos1_proj5_fwrisc_uart@4 44001a82-e59c-49f1-a214-40ce179027c6

---
+++ fwrisc_uart/fpga/fwrisc_uart_wraper.sv
@@ -0,0 +1,35 @@
+module fwrisc_uart_wraper(
+		input			clock,
+		input			reset,
+
+		input			rx,
+		output			tx,
+
+		output			program_receiving,
+		output			program_ov,
+		output			program_done
+);
+	wire clk;
+	
+	clk_wiz_0 u_clock_gen(
+		// Clock out ports
+		.clk_out1(clk),     // output clk_out1
+		// Status and control signals
+		.reset(reset), // input reset
+		.locked(),       // output locked
+		// Clock in ports
+		.clk_in1(clock));      // input clk_in1
+
+	
+	fwrisc_fpga_top u_fwrisc_fpga_top(
+		.clock		(clk),
+		.reset		(reset),		
+		.tx			(tx),
+		.rx			(rx),
+		.program_receiving(program_receiving),
+		.program_ov		  (program_ov),
+		.program_done	  (program_done)
+
+	);
+	
+endmodule 
---
+++ vivado_proj/fwrisc_uart.srcs/sim_1/new/pll_tb.sv
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/07/18 15:40:44
+// Design Name: 
+// Module Name: pll_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pll_tb;
+	reg clock;
+	reg reset;
+	wire clk;
+	
+	initial begin
+		clock = 1;
+		reset = 1;
+		
+		#100
+		reset = 0;
+	end
+	
+	always #4 clock = ~clock;
+
+	clk_wiz_0 u_clock_gen(
+		// Clock out ports
+		.clk_out1(clk),     // output clk_out1
+		// Status and control signals
+		.reset(reset), // input reset
+		.locked(),       // output locked
+		// Clock in ports
+		.clk_in1(clock));      // input clk_in1
+endmodule
