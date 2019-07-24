55f3f2228096e7f87abd9ef13893593c0a14b087
yuzhenbo <yuzhenbo@44001a82-e59c-49f1-a214-40ce179027c6>
2019-07-24 14:38:56 +0000

    git-svn-id: svn://144.34.249.81/repos0_proj/repos1_proj5_fwrisc_uart@25 44001a82-e59c-49f1-a214-40ce179027c6

--- vivado_proj/fwrisc_uart.runs/impl_3/rundef.js
+++ vivado_proj/fwrisc_uart.runs/impl_3/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log fwrisc_uart_wraper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source fwrisc_uart_wraper.tcl -notrace" );
 
